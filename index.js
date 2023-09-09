896da4f8febaeb757eb8473abec4af150cf503c3
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-09-09 16:06:59 +0500

    calculator completed

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-let fname = "Babar";
-
-function test() {
-  fname = "azam";
-  console.log(`${fname}`);
+// Function to append characters to the display
+function appendToDisplay(value) {
+  document.getElementById("display").value += value;
 }
-console.log(`${fname}`);
-test();
+
+// Function to append operators to the display
+function appendOperator(operator) {
+  const display = document.getElementById("display");
+  const currentValue = display.value;
+
+  // Check if the last character is an operator, and replace it with the new operator
+  if (
+    currentValue.charAt(currentValue.length - 1) === "+" ||
+    currentValue.charAt(currentValue.length - 1) === "-" ||
+    currentValue.charAt(currentValue.length - 1) === "*" ||
+    currentValue.charAt(currentValue.length - 1) === "/"
+  ) {
+    display.value = currentValue.slice(0, -1) + operator;
+  } else {
+    display.value += operator;
+  }
+}
+
+// Function to clear the display
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+// Function to calculate the result
+function calculate() {
+  const display = document.getElementById("display");
+  const expression = display.value;
+
+  try {
+    const result = eval(expression); // Using eval for simplicity (not recommended for production)
+    display.value = result;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
